4e48c04ec0a20b470cd16e996aaddf12d184e839
wjdwodnr5452 <wjdwodnr5452@naver.com>
2025-01-03 22:10:47 +0900

    Initial commit

---
+++ src/test/java/com/example/redisinspring/RedisInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisInSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
